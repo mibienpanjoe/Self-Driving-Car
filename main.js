dd5af29a851763da615e797fb078015764534cd3
mibienpanjoe <epixprint00@gmail.com>
2025-12-29 23:55:52 +0000

    feat: genetic algorithm based on mutation + add a detailed README

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const networkCtx = networkCanvas.getContext("2d");
 
 const road=new Road(carCanvas.width/2,carCanvas.width*0.9);
 
-const N=1;
+const N=200;
 const cars=generateCars(N);
 let bestCar=cars[0];
 if(localStorage.getItem("bestBrain")){
@@ -22,13 +22,13 @@ if(localStorage.getItem("bestBrain")){
 }
 
 const traffic=[
-    new Car(road.getLaneCenter(1),-100,30,50,"DUMMY",2),
-    new Car(road.getLaneCenter(0),-300,30,50,"DUMMY",2),
-    new Car(road.getLaneCenter(2),-300,30,50,"DUMMY",2),
-    new Car(road.getLaneCenter(0),-500,30,50,"DUMMY",2),
-    new Car(road.getLaneCenter(1),-500,30,50,"DUMMY",2),
-    new Car(road.getLaneCenter(1),-700,30,50,"DUMMY",2),
-    new Car(road.getLaneCenter(2),-700,30,50,"DUMMY",2),
+    new Car(road.getLaneCenter(1),-100,30,50,"DUMMY",2,getRandomColor()),
+    new Car(road.getLaneCenter(0),-300,30,50,"DUMMY",2,getRandomColor()),
+    new Car(road.getLaneCenter(2),-300,30,50,"DUMMY",2,getRandomColor()),
+    new Car(road.getLaneCenter(0),-500,30,50,"DUMMY",2,getRandomColor()),
+    new Car(road.getLaneCenter(1),-500,30,50,"DUMMY",2,getRandomColor()),
+    new Car(road.getLaneCenter(1),-700,30,50,"DUMMY",2,getRandomColor()),
+    new Car(road.getLaneCenter(2),-700,30,50,"DUMMY",2,getRandomColor()),
 ];
 
 animate();
@@ -64,20 +64,21 @@ function animate(time){
 
     carCanvas.height=window.innerHeight;
     networkCanvas.height=window.innerHeight;
+    networkCanvas.width = window.innerWidth/3;
 
     carCtx.save();
     carCtx.translate(0,-bestCar.y+carCanvas.height*0.7);
 
     road.draw(carCtx);
     for(let i=0;i<traffic.length;i++){
-        traffic[i].draw(carCtx,"red");
+        traffic[i].draw(carCtx);
     }
     carCtx.globalAlpha=0.2;
     for(let i=0;i<cars.length;i++){
-        cars[i].draw(carCtx,"blue");
+        cars[i].draw(carCtx);
     }
     carCtx.globalAlpha=1;
-    bestCar.draw(carCtx,"blue",true);
+    bestCar.draw(carCtx,true);
 
     carCtx.restore();
 
