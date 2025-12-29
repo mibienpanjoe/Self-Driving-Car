9df8146e2ea34e919d16c8db2b44e9102fb48252
mibienpanjoe <epixprint00@gmail.com>
2025-12-29 20:09:08 +0000

    feat : Add trafic simulation logic

--- controls.js
+++ controls.js
@@ -1,50 +1,52 @@
 class Controls{
-    constructor(){
-        this.forward = false;
-        this.left = false;
-        this.right = false;
-        this.reverse = false;
+    constructor(type){
+        this.forward=false;
+        this.left=false;
+        this.right=false;
+        this.reverse=false;
 
-        this.#addKeyboardListeners();
-
+        switch(type){
+            case "KEYS":
+                this.#addKeyboardListeners();
+                break;
+            case "DUMMY":
+                this.forward=true;
+                break;
+        }
     }
+
     #addKeyboardListeners(){
         document.onkeydown=(event)=>{
             switch(event.key){
-                case "ArrowUp":
-                    this.forward = true;
-                    break;
-                case "ArrowDown" :
-                    this.reverse = true;
+                case "ArrowLeft":
+                    this.left=true;
                     break;
                 case "ArrowRight":
-                    this.right =true;
+                    this.right=true;
                     break;
-                case "ArrowLeft" :
-                    this.left = true ; 
+                case "ArrowUp":
+                    this.forward=true;
+                    break;
+                case "ArrowDown":
+                    this.reverse=true;
                     break;
             }
-            console.table(this)
         }
-
         document.onkeyup=(event)=>{
             switch(event.key){
-                case "ArrowUp":
-                    this.forward = false;
-                    break;
-                case "ArrowDown" :
-                    this.reverse = false;
+                case "ArrowLeft":
+                    this.left=false;
                     break;
                 case "ArrowRight":
-                    this.right =false;
+                    this.right=false;
                     break;
-                case "ArrowLeft" :
-                    this.left = false ; 
+                case "ArrowUp":
+                    this.forward=false;
+                    break;
+                case "ArrowDown":
+                    this.reverse=false;
                     break;
             }
-            console.table(this)
         }
-            
-
     }
 }
